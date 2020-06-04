da9ad7a0fd6cfa38d80e530495bb6b6f750c88dc
Laila Campbell <lailacampbell96@gmail.com>
2020-06-04 19:55:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,12 @@
 #write your code here
 
 def countdown(num)
-  while num > 0
-  num +=1 
-  puts "#{number} SECOND(S)!"
-  num -= 1
-end 
-  "HAPPY NEW YEAR!"
-end 
+ while num > 0
+  puts "#{num} SECOND(S)!"
+ num -= 1
+ end
+'HAPPY NEW YEAR!'
+end
 
 # def countdown
 #   countdown = 0 
