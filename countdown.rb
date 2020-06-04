0d6072cca316e45f4e362ceaeff10fa05b4228d0
Laila Campbell <lailacampbell96@gmail.com>
2020-06-04 19:44:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,15 @@
 #write your code here
 
 def countdown
-  countdown = 0 
-  until countdown == 10
-  countdown=+1 
-  puts "#{number} SECOND(S)!"
-  puts "HAPPY NEW YEAR!"
+  
 end 
-end
-countdown
+
+# def countdown
+#   countdown = 0 
+#   until countdown == 10
+#   countdown=+1 
+#   puts "#{number} SECOND(S)!"
+#   puts "HAPPY NEW YEAR!"
+# end 
+# end
+# countdown
